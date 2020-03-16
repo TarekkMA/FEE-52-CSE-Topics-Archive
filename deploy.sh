c845cb753296a81f6cc62ddee8a465fe26da35f1
TarekkMA <tarekkma@gmail.com>
2020-03-16 18:06:12 +0200

    deploy

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,6 @@ yarn build
 git add -A
 git commit -m 'deploy'
 
-git push git@github.com:tarekkma/FEE-52-CSE-Topics-Archive.git master:gh-pages
+git push https://github.com:tarekkma/FEE-52-CSE-Topics-Archive.git master:gh-pages
 
 cd -
